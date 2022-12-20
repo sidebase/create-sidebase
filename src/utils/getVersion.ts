9f1549bf7b07f174b222ba33b49a1156f7c6abbe
BracketJohn <njonalik@protonmail.com>
2022-12-20 09:45:47 +0100

    fix: improve counting, wrap version getting in try/catch

--- src/utils/getVersion.ts
+++ src/utils/getVersion.ts
@@ -1,5 +1,4 @@
 import fetch from "node-fetch"
-import crypto from "crypto"
 import { cliOptions } from "./parseCliOptions"
 
 // Adapted from: https://github.com/withastro/astro/blob/2552816d5fa14a191a73179698b4b6f574a9963f/packages/create-astro/src/messages.ts#L44-L58
@@ -9,16 +8,26 @@ export const getVersion = async () => {
     return v
   }
 
-  if (!cliOptions.nocounting) {
-    fetch("https://plausible.io/api/event", {
-      method: "POST",
-      headers: { "User-Agent": "Mozilla/5.0", "X-Forwarded-For": "127.0.0.1" },
-      body: `{"name":"cli","url":"https://sidebase.io/sidebase-cli/${crypto.randomBytes(16).toString("hex")}","domain":"sidebase.io"}`
-    })
+  if (process.env.NODE_ENV === "production" && !cliOptions.nocounting) {
+    const randomIp = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))
+    try {
+      fetch("https://plausible.io/api/event", {
+        method: "POST",
+        headers: { "User-Agent": "Mozilla/5.0", "X-Forwarded-For": `${randomIp}` },
+        body: "{\"name\":\"cli\",\"url\":\"https://sidebase.io/sidebase/cli/healthz\",\"domain\":\"sidebase.io\"}"
+      })
+    } catch (error) {
+      // pass
+    }
   }
 
-  const response: { version: string } = await fetch("https://registry.npmjs.org/create-sidebase/latest").then(response => response.json() as unknown as { version: string } )
+  let response: { version: string }
+  try {
+    response = await fetch("https://registry.npmjs.org/create-sidebase/latest").then(response => response.json() as unknown as { version: string } )
+  } catch (error) {
+    response = { version: "0.0.0"}
+  }
 
   v = response.version
-  return response.version
+  return v
 }
