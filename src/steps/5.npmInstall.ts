5b4ab1d5ae2a97a88c5704ffc94b90d963bb5c58
BracketJohn <njonalik@protonmail.com>
2023-01-10 17:59:16 +0100

    feat: add step that automatically lint & fix all files to avoid problems from raw template strings

--- src/steps/5.npmInstall.ts
+++ src/steps/5.npmInstall.ts
@@ -1,4 +1,7 @@
 import { getUserPkgManager } from "../utils/getUserPkgManager"
 import { execa } from "execa"
 
-export default (templateDir: string) => execa(getUserPkgManager(), ["install"], { cwd: templateDir, env: { NODE_ENV: "development" } })
+const install = (templateDir: string) => execa(getUserPkgManager(), ["install"], { cwd: templateDir, env: { NODE_ENV: "development" } })
+const lintAndFix = (templateDir: string) => execa(getUserPkgManager(), ["run", "lint", "--", "--fix"], { cwd: templateDir, env: { NODE_ENV: "development" } })
+
+export default (templateDir: string) => install(templateDir).then(() => lintAndFix(templateDir))
