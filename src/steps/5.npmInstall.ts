c0b9c0fd7a8700170cd069144f12fcdeb23d1166
BracketJohn <njonalik@protonmail.com>
2023-01-11 10:38:29 +0100

    fix: fix snippets instead of automatically fixing in the end

--- src/steps/5.npmInstall.ts
+++ src/steps/5.npmInstall.ts
@@ -1,7 +1,4 @@
 import { getUserPkgManager } from "../utils/getUserPkgManager"
 import { execa } from "execa"
 
-const install = (templateDir: string) => execa(getUserPkgManager(), ["install"], { cwd: templateDir, env: { NODE_ENV: "development" } })
-const lintAndFix = (templateDir: string) => execa(getUserPkgManager(), ["run", "lint", "--", "--fix"], { cwd: templateDir, env: { NODE_ENV: "development" } })
-
-export default (templateDir: string) => install(templateDir).then(() => lintAndFix(templateDir))
+export default (templateDir: string) => execa(getUserPkgManager(), ["install"], { cwd: templateDir, env: { NODE_ENV: "development" } })
