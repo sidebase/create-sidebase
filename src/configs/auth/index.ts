36947bb859868e1c4b00a29122f12d287e23501a
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-03-30 00:59:05 +0100

    fixed nuxt auth index page

--- src/configs/auth/index.ts
+++ src/configs/auth/index.ts
@@ -13,7 +13,7 @@ export default NuxtAuthHandler({
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
       clientId: process.env.GITHUB_CLIENT_ID || 'enter-your-client-id-here',
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || 'enter-your-client-secret-here' // TODO: Replace this with an env var like "process.env.GITHUB_CLIENT_SECRET". The secret should never end up in your github repository
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || 'enter-your-client-secret-here', // TODO: Replace this with an env var like "process.env.GITHUB_CLIENT_SECRET". The secret should never end up in your github repository
     }),
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     CredentialsProvider.default({
@@ -25,9 +25,9 @@ export default NuxtAuthHandler({
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         username: { label: 'Username', type: 'text', placeholder: '(hint: jsmith)' },
-        password: { label: 'Password', type: 'password', placeholder: '(hint: hunter2)' }
+        password: { label: 'Password', type: 'password', placeholder: '(hint: hunter2)' },
       },
-      authorize (credentials: any) {
+      authorize(credentials: any) {
         console.warn('ATTENTION: You should replace this with your real providers or credential provider logic! The current setup is not safe')
         // You need to provide your own logic here that takes the credentials
         // submitted and returns either a object representing a user or value
@@ -39,7 +39,8 @@ export default NuxtAuthHandler({
         if (credentials?.username === user.username && credentials?.password === user.password) {
           // Any object returned will be saved in \`user\` property of the JWT
           return user
-        } else {
+        }
+        else {
           console.error('Warning: Malicious login attempt registered, bad credentials provided')
 
           // If you return null then an error will be displayed advising the user to check their details.
@@ -47,9 +48,9 @@ export default NuxtAuthHandler({
 
           // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
-      }
-    })
-  ]
+      },
+    }),
+  ],
 })
 `
 
