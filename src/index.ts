02167bdbc79f8455d13ff7c50b1a5f5049c6f9c6
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-08-19 11:46:34 +0200

    Add i18n to default CI options

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ async function main() {
   let preferences: Preferences = {
     setProjectName: 'my-sidebase-app',
     setStack: 'merino',
-    addModules: ['prisma', 'sidebase-auth', 'trpc', 'tailwind', 'naiveui'],
+    addModules: ['prisma', 'sidebase-auth', 'trpc', 'tailwind', 'naiveui', 'i18n'],
     runGitInit: true,
     addCi: 'github',
     runInstall: true
